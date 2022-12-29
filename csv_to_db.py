2b757352f9dd6eb7493ab716fac8943ccd23a6a7
Mateusz <nnn.2321a@gmail.com>
2022-12-29 19:11:32 +0100

    adding models for students, teachers and issues

--- csv_to_db.py
+++ csv_to_db.py
@@ -6,6 +6,7 @@ with open('google_books_dataset.csv', 'r') as f:
     rows = list(reader)
 
 for row in rows[1:]:
-    book = Books(title=row[0], authors=row[1], language=row[2], categories=row[3], avg_rate=int(float(row[4])) if row[4] !='' else row[4])
+    book = Books(title=row[0], authors=row[1], language=row[2], categories=row[3],
+                 avg_rate=int(float(row[4])) if row[4] != '' else row[4])
     db.session.add(book)
 db.session.commit()
